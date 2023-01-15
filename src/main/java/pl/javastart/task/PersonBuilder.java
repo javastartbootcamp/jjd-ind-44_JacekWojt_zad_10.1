6d51ac06ed69d9efc27b656f0457a7114ad69a00
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-01-15 20:21:20 +0100

    Rozwiązanie

---
+++ src/main/java/pl/javastart/task/PersonBuilder.java
@@ -0,0 +1,32 @@
+package pl.javastart.task;
+
+import java.util.Scanner;
+
+public class PersonBuilder {
+    private Scanner scanner = new Scanner(System.in);
+
+    private Person createPerson() {
+        System.out.print("Podaj imię: ");
+        String firstname = scanner.nextLine();
+        System.out.print("Podaj wiek: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        return new Person(firstname, age);
+    }
+
+    Person[] createPersonArray(int size) {
+        Person[] persons = new Person[size];
+        int i = 0;
+        persons[i] = createPerson();
+        do {
+            i++;
+            persons[i] = createPerson();
+            while (persons[i - 1].equals(persons[i])) {
+                System.out.println("Duplikat");
+                persons[i] = createPerson();
+            }
+        } while (i < size - 1);
+        return persons;
+    }
+}
+
